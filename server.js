0527fdbe4af0499e40bb55b7aa3844ce16a071e3
almas998 <almas.haneef@asinlabs.tech>
2022-12-05 12:45:04 +0500

    changes

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.use(express.json());
 app.use("/api" , userRoute );
 app.use(express.static('public'));
 // app.use(express.static(__dirname+"./uploads"))
-// app.get('/', (req, res) => {
-//     res.send('Hello World!')
-//   })
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
